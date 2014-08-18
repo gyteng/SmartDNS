43b2e18deb7034b060eb63374abce2a55cebcdad
gyteng <igyteng@gmail.com>
2014-08-18 15:04:08 +0800

    Add config.js

--- dns.js
+++ dns.js
@@ -1,14 +1,13 @@
 var PORT = 53;
 var HOST = '0.0.0.0';
-var DNS = '8.8.8.8';
+//var DNS = ['114.114.114.114'];
+var DNS = ['202.96.128.86', '114.114.114.114', '8.8.8.8'];
 var dgram = require('dgram');
 var bu = require('./BufferUtils');
+var config = require('./config');
 var server = dgram.createSocket('udp4');
 var client = dgram.createSocket('udp4');
-var fakeIpList = [
-    '59.24.3.173',
-    '37.61.54.158'];
-
+var fakeIpList = config.fakeIpList;
 
 server.on('listening', function () {
     var address = server.address();
@@ -23,32 +22,33 @@ server.on('message', function (messageReq, remoteReq) {
     bu.printBuffer(messageReq);
     getDomain(messageReq);
 
-    var client = dgram.createSocket('udp4');
-    client.send(messageReq, 0, messageReq.length, 53, DNS, function(err, bytes) {
-        if (err) throw err;
-        console.log('Message send to ' + DNS + ':' + 53);
-        console.log('-------------------------------------------');
-
-        client.on("message", function (messageRes, remoteRes) {
-            console.log('Receive: ' + remoteRes.address + ':' + remoteRes.port);
-            console.log('Length: ' + messageRes.length);
-            bu.printBuffer(messageRes);
-            var fakeIp = getFakeIp(messageRes);
-
-            if(isFakeIp(fakeIp, fakeIpList)) {
-                console.log('Fake ip');
-                // Do nothing;
+    for(var i in DNS) {
+        (function(i) {
+            var client = dgram.createSocket('udp4');
+            client.send(messageReq, 0, messageReq.length, 53, DNS[i], function (err, bytes) {
+                if (err) throw err;
+                console.log('Message send to ' + DNS[i] + ':' + 53);
                 console.log('-------------------------------------------');
-            } else {
-                server.send(messageRes, 0, messageRes.length, remoteReq.port, remoteReq.address, function (err, bytes) {
-                    if (err) throw err;
-                    console.log('Message send to ' + remoteReq.address + ':' + remoteReq.port);
-                    console.log('-------------------------------------------');
+                client.on("message", function (messageRes, remoteRes) {
+                    console.log('Receive: ' + remoteRes.address + ':' + remoteRes.port);
+                    console.log('Length: ' + messageRes.length);
+                    bu.printBuffer(messageRes);
+                    //var fakeIp = getFakeIp(messageRes);
+                    var ip = getIp(messageRes);
+                    if (isFakeIp(ip[0], fakeIpList)) {
+                        console.log('Fake ip');
+                        console.log('-------------------------------------------');
+                    } else {
+                        server.send(messageRes, 0, messageRes.length, remoteReq.port, remoteReq.address, function (err, bytes) {
+                            if (err) throw err;
+                            console.log('Message send to ' + remoteReq.address + ':' + remoteReq.port);
+                            console.log('-------------------------------------------');
+                        });
+                    }
                 });
-            }
-        });
-        //client.close();
-    });
+            });
+        })(i);
+    }
 
 });
 
@@ -77,31 +77,42 @@ function getFakeIp(buffer) {
 }
 
 function isFakeIp(ip, list) {
-    var rtn = false;
-    for(var l in list) {
-        if(ip == list[l]) {
+    if(list.length == 0) {
+        return false;
+    }
+    for(var i in list) {
+        if(ip == list[i]) {
             return true;
         }
     }
     return false;
 }
-/*
-function getRealIp(buffer) {
 
+function getIp(buffer) {
+    var ipList = [];
     var ipAddress = '';
     var offset = 12;
     while(buffer[offset] != 0) {
         offset += (buffer[offset] + 1);
     }
-    offset += 17;
+ /*   if(buffer[offset + 5] != 192) {
+        offset += 5;
+        while(buffer[offset] != 0) {
+            offset += (buffer[offset] + 1);
+        }
+        offset -= 6;
+    }
+*/    offset += 17;
     while(offset < buffer.length) {
         ipAddress = '';
         ipAddress += buffer[offset] + '.' + buffer[offset+1] + '.' + buffer[offset+2] + '.' + buffer[offset+3];
-        console.log(ipAddress);
+    //    console.log(ipAddress);
+        ipList.push(ipAddress);
         offset += 16;
     }
+    console.log(ipList);
+    return ipList;
 }
-*/
 
 server.bind(PORT, HOST);
 
